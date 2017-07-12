9492ee6184f4d80f7dc2afed7b98d3671dbeb4f4
Skyridge Devcamp <skyridge@devcamp.com>
2017-07-12 17:42:39 +0000

    Customized registration, login, and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :portfolios
   get 'angular-items', to:'portfolio#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
@@ -15,5 +15,5 @@ Rails.application.routes.draw do
     end
   end
 
-root to: 'pages#home'
+  root to: 'pages#home'
 end
